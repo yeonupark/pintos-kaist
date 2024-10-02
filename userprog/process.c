a02e805d317649908c85abb61550e862493b5e3d
Ubuntu <ubuntu@ip-172-31-58-111.ap-northeast-2.compute.internal>
2024-10-02 07:10:11 +0000

    passing complete

--- userprog/process.c
+++ userprog/process.c
@@ -44,7 +44,7 @@ process_create_initd (const char *file_name) {
 	tid_t tid;
 	char *token;
     char *save_ptr;
-	printf("f_name1: %s\n" ,*(&file_name));
+	// printf("f_name1: %s\n" ,*(&file_name));
 	/* Make a copy of FILE_NAME.
 	 * Otherwise there's a race between the caller and load(). */
 	fn_copy = palloc_get_page (0);
@@ -52,10 +52,10 @@ process_create_initd (const char *file_name) {
 		return TID_ERROR;
 	strlcpy (fn_copy, file_name, PGSIZE);
 	token = strtok_r(file_name, " ", &save_ptr);	
-	printf("f_name2: %s\n" ,*(&file_name));
+	// printf("f_name2: %s\n" ,*(&file_name));
 	/* Create a new thread to execute FILE_NAME. */
 	tid = thread_create (file_name, PRI_DEFAULT, initd, fn_copy);
-	printf("tid = %d\n",tid);
+	// printf("tid = %d\n",tid);
 	if (tid == TID_ERROR)
 		palloc_free_page (fn_copy);
 	return tid;
@@ -64,16 +64,16 @@ process_create_initd (const char *file_name) {
 /* A thread function that launches first user process. */
 static void
 initd (void *f_name) {
-	printf("imitd\n");
+	// printf("imitd\n");
 #ifdef VM
 	supplemental_page_table_init (&thread_current ()->spt);
 #endif
-	printf("imitd1\n");
+	// printf("imitd1\n");
 	process_init ();
-	printf("imitd2\n");
+	// printf("imitd2\n");
 	if (process_exec (f_name) < 0)
 		PANIC("Fail to launch initd\n");
-	printf("imitd3\n");
+	// printf("imitd3\n");
 	NOT_REACHED ();
 }
 
@@ -172,8 +172,8 @@ int
 process_exec (void *f_name) {  
 	char *file_name = f_name;	
 	bool success;
-	printf("f_name3: %s\n" ,*(&f_name));
-	printf("finame: %p\n" ,file_name);
+	// printf("f_name3: %s\n" ,*(&f_name));
+	// printf("finame: %p\n" ,file_name);
 	
 	/* We cannot use the intr_frame in the thread structure.
 	 * This is because when current thread rescheduled,
@@ -339,7 +339,7 @@ load (const char *file_name, struct intr_frame *if_) {
 	off_t file_ofs;
 	bool success = false;
 	int i;
-	printf("f_name4: %s\n" ,*(&file_name));
+	// printf("f_name4: %s\n" ,*(&file_name));
 	char *args[10];
 	int arg_count = 0;
 	char *token;
@@ -351,9 +351,9 @@ load (const char *file_name, struct intr_frame *if_) {
     	args[arg_count++] = token;
     	token = strtok_r(NULL, " ", &save_ptr);
 	}
-	for (int i = 0; i < arg_count; i++) {
-    	printf("Arg%d: %s\n", i, args[i]);
-	}
+	// for (int i = 0; i < arg_count; i++) {
+    // 	printf("Arg%d: %s\n", i, args[i]);
+	// }
 	/* Allocate and activate page directory. */
 	t->pml4 = pml4_create ();
 	if (t->pml4 == NULL)
@@ -440,23 +440,33 @@ load (const char *file_name, struct intr_frame *if_) {
 
 	/* TODO: Your code goes here.
 	 * TODO: Implement argument passing (see project2/argument_passing.html). */
-	printf("rsp = %p\n" , if_->rsp);
+	// printf("rsp = %p\n" , if_->rsp);
 	for(int i = arg_count-1 ; i>=0;i--){
 		size_t arg_len = strlen(args[i]) + 1;
 		if_->rsp -= arg_len;
 		memcpy(if_->rsp,args[i],arg_len);
-		printf("arg=%s rsp = %p\n" ,args[i], if_->rsp);
-		printf("stack arg=%s\n",*(&if_->rsp));
+		// printf("arg=%s rsp = %p\n" ,args[i], if_->rsp);
+		args[i] = if_->rsp;
 	}
-	int mod = if_->rsp % 8;
-	if (mod != 0){
-		uint8_t word_align = 0;
+	int mod = if_->rsp % 8;  // 8바이트 정렬을 위해 나머지 계산
+	if (mod != 0) {
 		if_->rsp -= mod;
-		memcpy(if_->rsp,word_align,mod);
+		uint8_t word_align[8] = {0};  
+		memcpy(if_->rsp, word_align, mod);  
 	}
-	printf("align rsp %p\n", if_->rsp);
-
-
+	// printf("after align rsp %p\n", if_->rsp);
+	if_->rsp -= sizeof(char *);  
+	memset(if_->rsp, 0, sizeof(char *));  
+	for (int i = arg_count - 1; i >= 0; i--) {
+		if_->rsp -= sizeof(char *);  
+		memcpy(if_->rsp, &args[i], sizeof(char *));  
+		// printf("argv[%d] = %p\n", i, *(char **)if_->rsp); 
+	}
+	char **argv = if_->rsp;  
+	if_->rsp -= sizeof(void *); 
+	memset(if_->rsp, 0, sizeof(void *));  
+	// printf("final rsp = %p\n", if_->rsp);
+	hex_dump((uintptr_t)if_->rsp, (void *)if_->rsp, USER_STACK - (uintptr_t)if_->rsp, true);
 	success = true;
 
 done:
